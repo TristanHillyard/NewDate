24cf3f222d177780ac47469046657908e6e1cddc
TristanHillyard <73565670+TristanHillyard@users.noreply.github.com>
2021-01-03 16:24:27 -0700

    Add project files.

---
+++ NewDate/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace NewDate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine("Enter a number");
+            double hours = Convert.ToInt32(Console.ReadLine());
+            DateTime date = DateTime.Now;
+            DateTime date1 = date.AddHours(hours);
+
+            Console.WriteLine("It will be {0} in {1} hours", date1, hours);
+        }
+    }
+}
